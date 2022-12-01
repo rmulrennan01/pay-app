ddb86a4e0fb4e6c6c84413b430fbe442363e8fc4
rmulrennan01 <rpmulrennan@gmail.com>
2022-12-01 14:57:28 -0600

    building pay app table inside contract page

--- src/Contract_page/Pay_app_table.js
+++ src/Contract_page/Pay_app_table.js
@@ -14,12 +14,16 @@ import CurrencyFormat from 'react-currency-format';
 
 
 function Pay_app_table(props) {
-  const [pay_apps, set_pay_apps] = useState([]);
+  const [pay_apps, set_pay_apps] = useState(props.sov);
   const [co_apps, set_co_apps] = useState([]);
-  const table_headers = ["Base Contract", "Change Orders", "Previous Payments", "Payment This Period", "Remaining Balance", "Retention"];
+  const table_headers = ["Base Contract", "Change Orders", "Revised Contract",  "Previous Payments", "Payment This Period", "Remaining Balance", "Retention"];
+  const [period_info, set_period_info] = useState([]); 
 
+
+  //contract_info={contract_info} sov={sov}
 
   useEffect(() => {
+    build_period_totals()
     
 
 
@@ -27,28 +31,93 @@ function Pay_app_table(props) {
   }, []); 
 
     //calculate total of CO's per app period
-    const get_co_apps = () => {
-        let temp_co_list = []; 
+    const build_period_totals = () => {
 
+        let temp_app_totals = new Array(props.contract_info.app_count+1).fill(0); 
+        let temp_co_totals = new Array(props.contract_info.app_count+1).fill(0); 
+       
+        //loop through each sov item
+        for (let i = 0; i < props.sov.length; i++){
+          let temp_sov_item = props.sov[i]; 
+          console.log("temp sov item is: ", temp_sov_item); 
+        
+          //loop through each SOV item and create a combined draw total for each pay period          
+          if(temp_sov_item.pay_apps !==0){
+            temp_sov_item.pay_apps.map((item,index) => temp_app_totals[index] = Number(temp_app_totals[index])+Number(item))
+          }
+          
+          //loop through each CO for the sov item to build totals for each pay period
+          if(temp_sov_item.change_orders !==0){
+            temp_sov_item.change_orders.map((item) => temp_co_totals[item.pay_app-1] = Number(temp_co_totals[item.pay_app-1])+Number(item.value))
+          }
+        }
+
+        let temp_line_item = []; 
+        for (let i = 0; i<temp_app_totals.length; i++){
+          let temp_info = {base_contract:0, change_orders:0, revised_contract:0, this_draw:0, previous_payments:0, balance:0, retention:0}; 
+          temp_info.base_contract=props.contract_info.base_contract_value;
+          temp_info.change_orders=temp_co_totals[i];
+          
+          temp_info.this_draw=temp_app_totals[i]; 
+          if(i==0){
+            temp_info.previous_payments=0;
+            temp_info.revised_contract=Number(props.contract_info.base_contract_value)+Number(temp_co_totals[i]); 
+          }
+          else{
+            temp_info.previous_payments=Number(temp_line_item[i-1].this_draw)+Number(temp_line_item[i-1].previous_payments); 
+            temp_info.revised_contract=Number(temp_line_item[i-1].revised_contract)+Number(temp_co_totals[i]); 
+          }
+          temp_info.balance=Number(temp_info.revised_contract)-Number(temp_info.previous_payments)-Number(temp_info.this_draw);
+        
+          temp_line_item.push(temp_info); 
+        }
+        
+        console.log("temp line item", temp_line_item); 
+        console.log("temp_app_totals", temp_app_totals); 
+        console.log("temp_co_totals", temp_co_totals); 
+        console.log("contract_info", props.contract_info); 
+        set_period_info(temp_line_item); 
+        //period_info=temp_line_item; 
 
     }
   
 
-    const build_table_body = (item, index) => {
+    const build_table_body = (item) => {
+      return(
+      //let temp_info = {base_contract:0, change_orders:0, revised_contract:0, this_draw:0, previous_payments:0, balance:0, retention:0};
       <TableRow>
         <TableCell> 
-          <CurrencyFormat value={item.value} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
-
-        
+          <CurrencyFormat value={item.base_contract} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
         </TableCell>
-
+        <TableCell>
+          <CurrencyFormat value={item.change_orders} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        <TableCell> 
+          <CurrencyFormat value={item.revised_contract} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        <TableCell> 
+          <CurrencyFormat value={item.previous_payments} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        <TableCell> 
+          <CurrencyFormat value={item.this_draw} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        <TableCell> 
+          <CurrencyFormat value={item.balance} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        <TableCell> 
+          <CurrencyFormat value={item.retention} displayType={'text'} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={2}/>
+        </TableCell>
+        {console.log("here", item)}
       </TableRow>
+      )
     }
 
+  
 
 
   return (
     <div>
+      
       <Button variant="contained" onClick={()=>window.location='/pay_app/'+ String(props.id)}> Create Application </Button>
         <Table>
           <TableHead> 
@@ -56,6 +125,16 @@ function Pay_app_table(props) {
               {table_headers.map((item) => <TableCell> {item} </TableCell> )}
             </TableRow>
           </TableHead>
+          <TableBody>
+            
+            
+            {(pay_apps.length == 0) ? console.log("it's here") : period_info.map(build_table_body)}
+
+            
+            
+            
+          </TableBody>
+
         </Table>
 
     
