1ac6e4850a03c568eedcbd5c189e0235e44cf221
rmulrennan01 <rpmulrennan@gmail.com>
2023-01-12 16:32:38 -0600

    fixing submission issues with change orders no correctly updating the balance in the contract_info

--- src/Contract_page.js
+++ src/Contract_page.js
@@ -92,15 +92,7 @@ function Contract_page(props) {
 
     const submit_co = (sov_id, data) => {
         //add the change order to the appropriate sov data item
-        /*let temp_app_count = 0; 
-        if(contract_info.app_count ==null){
-            temp_app_count =0; 
-
-        }
-        else{
-            temp_app_count = contract_info.app_count; 
-        }
-        */
+        /*
         firestoreDB.collection("contracts").doc(id).collection("sov").doc(sov_id).update({
 
             "change_orders": firebase.firestore.FieldValue.arrayUnion({description: data.description, value: data.value, pay_app: data.pay_app})
@@ -125,6 +117,35 @@ function Contract_page(props) {
         .catch((error) => {
             console.error("Error updating change order total", error); 
         });
+        */
+
+        let rev_contract = Number(contract_info.base_contract_value) + Number(contract_info.co_value) + Number(data.value); 
+        let balance = Number(rev_contract) - Number(contract_info.prev_draws) - Number(contract_info.this_draw); 
+        
+        let batch = firestoreDB.batch(); 
+        let contract_ref = firestoreDB.collection("contracts").doc(id); 
+        let sov_ref = contract_ref.collection("sov").doc(sov_id);
+    
+        //BATCH UPDATE WITH THE CO -> MUST UPDATE CO_COUNT; CO_VALUE; BALANCE; CO INSIDE SOV
+
+        batch.update(sov_ref, {"change_orders": firebase.firestore.FieldValue.arrayUnion({description: data.description, value: Number(data.value), pay_app: Number(data.pay_app)})});//DONE
+        batch.update(contract_ref, {"co_value":Number(contract_info.co_value)+Number(data.value)}); //DONE
+        batch.update(contract_ref, {"co_count":Number(contract_info.co_count)+Number(1)}); //DONE
+        batch.update(contract_ref, {"balance":Number(balance)}); //DONE
+
+        batch.commit().then(()=>{
+            console.log("updated co total successfully"); 
+            alert("Change Order Added Successfully"); 
+            window.location.reload(false);
+        })
+        .catch((error) => {
+            console.error("Error adding change order", error); 
+            alert("Failed to submit change order. Please try again later or contact support.")
+        });
+
+
+
+        
     } 
 
 
